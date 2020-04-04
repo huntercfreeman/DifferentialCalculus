b43a1dd60daf49663303a9c93f42a2b4b93a43f2
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-04-04 11:44:59 -0400

    a

---
+++ DifferentialCalculus/Shared/SideMenuChapter.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DifferentialCalculus.Shared
+{
+    public partial class SideMenuChapter : ComponentBase
+    {
+        [Parameter]
+        public string SectionTitle { get; set; }
+    }
+}
