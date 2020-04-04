fef5e47869f1d45890e7e5f7a8418df5dea00031
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-04-04 00:29:01 -0400

    Add project files.

---
+++ DifferentialCalculus/Shared/SideMenu.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DifferentialCalculus.Shared
+{
+    public partial class SideMenu : ComponentBase
+    {
+        [Inject]
+        public SiteState SiteState { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            SiteState.DisplaySideMenuEventHandler += SiteState_DisplaySideMenuEventHandler;
+        }
+
+        private void SiteState_DisplaySideMenuEventHandler(object sender, EventArgs e)
+        {
+            InvokeAsync(StateHasChanged);
+        }
+    }
+}
---
+++ DifferentialCalculus/Shared/SiteState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DifferentialCalculus.Shared
+{
+    public class SiteState
+    {
+        private bool _displaySideMenu;
+        public bool DisplaySideMenu 
+        {
+            get => _displaySideMenu;
+            set
+            {
+                _displaySideMenu = value;
+                DisplaySideMenuEventInvoke(new EventArgs());
+            }
+        }
+        public event EventHandler DisplaySideMenuEventHandler;
+        public void DisplaySideMenuEventInvoke(EventArgs e)
+        {
+            EventHandler handler = DisplaySideMenuEventHandler;
+            handler?.Invoke(this, e);
+        }
+    }
+}
