49ad3135ccceeef10afb58fff8cab3212799d671
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-04-04 15:21:07 -0400

    a

--- DifferentialCalculus/Repositories/ProblemRepository.cs
+++ DifferentialCalculus/Repositories/ProblemRepository.cs
@@ -804,6 +804,43 @@ namespace DifferentialCalculus.Repositories
                 new Problem { Number = 54 },
                 new Problem { Number = 55 }
             });
+            _sectionTitleProblems.Add("2.8", new List<Problem> 
+            {
+                new Problem { Number = 1 },
+                new Problem { Number = 2 },
+                new Problem { Number = 3 },
+                new Problem { Number = 4 },
+                new Problem { Number = 5 },
+                new Problem { Number = 6 },
+                new Problem { Number = 7 },
+                new Problem { Number = 8 },
+                new Problem { Number = 9 },
+                new Problem { Number = 10 },
+                new Problem { Number = 11 },
+                new Problem { Number = 12 },
+                new Problem { Number = 13 },
+                new Problem { Number = 14 },
+                new Problem { Number = 15 },
+                new Problem { Number = 16 },
+                new Problem { Number = 17 },
+                new Problem { Number = 18 },
+                new Problem { Number = 19 },
+                new Problem { Number = 20 },
+                new Problem { Number = 21 },
+                new Problem { Number = 22 },
+                new Problem { Number = 23 },
+                new Problem { Number = 24 },
+                new Problem { Number = 25 },
+                new Problem { Number = 26 },
+                new Problem { Number = 27 },
+                new Problem { Number = 28 },
+                new Problem { Number = 29 },
+                new Problem { Number = 30 },
+                new Problem { Number = 31 },
+                new Problem { Number = 32 },
+                new Problem { Number = 33 },
+                new Problem { Number = 34 }
+            });
         }
 
 
