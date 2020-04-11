ccef8d7d2436855bb53de2e2a26c023d481cd73f
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-04-10 22:48:13 -0400

    a

---
+++ DifferentialCalculus/Shared/PreviousQuestion.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DifferentialCalculus.Shared
+{
+    public partial class PreviousQuestion : ComponentBase
+    {
+        [Inject]
+        public SiteState SiteState { get; set; }
+    }
+}
