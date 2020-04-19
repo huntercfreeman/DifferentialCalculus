99fa44a456aa5410d5bdd0b6a52331600ce36e70
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-04-19 09:41:55 -0400

    a

---
+++ DifferentialCalculus/Shared/DisplayCurrentBook.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DifferentialCalculus.Shared
+{
+    public partial class DisplayCurrentBook : ComponentBase, IDisposable
+    {
+        [Inject]
+        public SiteState SiteState { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            SiteState.CurrentBookEventHandler += SiteState_CurrentBookEventHandler;
+        }
+
+        private void SiteState_CurrentBookEventHandler(object sender, EventArgs e)
+        {
+            InvokeAsync(StateHasChanged);
+        }
+        public void Dispose()
+        {
+            SiteState.CurrentBookEventHandler -= SiteState_CurrentBookEventHandler;
+        }
+    }
+}
